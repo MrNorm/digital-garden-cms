a59a7c72ddbff58c1e638eb8e78fcd8cdc110721
Oliver Northam <hi@oli.me>
2023-07-25 07:39:44 +0100

    Updated migrations to use postgres

---
+++ migrations/20230725063834_initial_db/migration.sql
@@ -0,0 +1,125 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL DEFAULT '',
+    "email" TEXT NOT NULL DEFAULT '',
+    "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Project" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL DEFAULT '',
+    "content" JSONB NOT NULL DEFAULT '[{"type":"paragraph","children":[{"text":""}]}]',
+    "createdAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Project_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Cutting" (
+    "id" TEXT NOT NULL,
+    "content" JSONB NOT NULL DEFAULT '[{"type":"paragraph","children":[{"text":""}]}]',
+    "createdAt" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Cutting_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Image" (
+    "id" TEXT NOT NULL,
+    "description" TEXT NOT NULL DEFAULT '',
+    "image_filesize" INTEGER,
+    "image_extension" TEXT,
+    "image_width" INTEGER,
+    "image_height" INTEGER,
+    "image_id" TEXT,
+
+    CONSTRAINT "Image_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Tag" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL DEFAULT '',
+
+    CONSTRAINT "Tag_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_Project_tags" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "_Cutting_images" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "_Cutting_tags" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "_Image_tags" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_Project_tags_AB_unique" ON "_Project_tags"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_Project_tags_B_index" ON "_Project_tags"("B");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_Cutting_images_AB_unique" ON "_Cutting_images"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_Cutting_images_B_index" ON "_Cutting_images"("B");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_Cutting_tags_AB_unique" ON "_Cutting_tags"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_Cutting_tags_B_index" ON "_Cutting_tags"("B");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_Image_tags_AB_unique" ON "_Image_tags"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_Image_tags_B_index" ON "_Image_tags"("B");
+
+-- AddForeignKey
+ALTER TABLE "_Project_tags" ADD CONSTRAINT "_Project_tags_A_fkey" FOREIGN KEY ("A") REFERENCES "Project"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_Project_tags" ADD CONSTRAINT "_Project_tags_B_fkey" FOREIGN KEY ("B") REFERENCES "Tag"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_Cutting_images" ADD CONSTRAINT "_Cutting_images_A_fkey" FOREIGN KEY ("A") REFERENCES "Cutting"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_Cutting_images" ADD CONSTRAINT "_Cutting_images_B_fkey" FOREIGN KEY ("B") REFERENCES "Image"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_Cutting_tags" ADD CONSTRAINT "_Cutting_tags_A_fkey" FOREIGN KEY ("A") REFERENCES "Cutting"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_Cutting_tags" ADD CONSTRAINT "_Cutting_tags_B_fkey" FOREIGN KEY ("B") REFERENCES "Tag"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_Image_tags" ADD CONSTRAINT "_Image_tags_A_fkey" FOREIGN KEY ("A") REFERENCES "Image"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_Image_tags" ADD CONSTRAINT "_Image_tags_B_fkey" FOREIGN KEY ("B") REFERENCES "Tag"("id") ON DELETE CASCADE ON UPDATE CASCADE;
